41f9f04f1ffd9ceea56a23955c4ab66db1f2bd45
Maykr1 <eclark72713@gmail.com>
2024-09-17 08:33:30 -0400

    Added .gitignore and made some changes to insultor.py. Added 100 passive-aggressive insults.

--- insultor.py
+++ insultor.py
@@ -1,7 +1,13 @@
+import random
+
 def insultor(password):
-    with open("PasswordGeneRate/passwords.txt", "r") as file:
+    with open("PasswordGeneRate/text_files/passwords.txt", "r") as file:
         lines = file.readlines()
         for line in lines:
             if password == line.strip():
-                return "BAD PASSWORD"
-        return "good"
+                with open("PasswordGeneRate/text_files/insults.txt", "r") as insults:
+                    return random.choice(insults.readlines())                  
+        return "good password"
+    
+#Open passwords.txt as file
+#If password is in file, open insults.txt and grab a random insult
