de5e6328c96b4abadea502a52484bf1fcbf939df
Maykr1 <eclark72713@gmail.com>
2024-10-15 12:53:53 -0400

    Created Demo Front End

--- PasswordGeneRate/PasswordGeneRateApp/urls.py
+++ PasswordGeneRate/PasswordGeneRateApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("generator/", views.generator, name="generator"),
     path("insultor/", views.insultor, name="insultor"),
+    path("privacy_policy/", views.privacy, name="privacy"),
 ]
 #update each time a new function is made in views.py
--- PasswordGeneRate/PasswordGeneRateApp/views.py
+++ PasswordGeneRate/PasswordGeneRateApp/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 
+password_criteria = ["4 characters", "1 uppercase letter", "1 lowercase letter", "1 number", "1 special character (!, @, $, etc.)"]
+
 # Create your views here.
 def home(request):
     context = {
-        "name": "Ethan",
-        "age" : "20"
+        "name": "Home"
     }
 
     return render(request, 'PasswordGeneRateApp/home.html', context)
 
 def generator(request):
     context = {
-        "name": "Bob"
+        "name": "Generator",
+        "criteria": password_criteria
     }
 
     return render(request, 'PasswordGeneRateApp/generator.html', context)
 
 def insultor(request):
     context = {
-        "name": "Jeff"
+        "name": "Insultor"
     }
 
     return render(request, 'PasswordGeneRateApp/insultor.html', context)
+
+def privacy(request):
+    return render(request, "PasswordGeneRateApp/privacy.html")
 #update each time a new html is made
