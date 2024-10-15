cf3b1c1efb1ed56c58e3300a7ce052d9dee97975
Maykr1 <eclark72713@gmail.com>
2024-10-15 17:30:24 -0400

    Starting attempt to populate database

--- PasswordGeneRate/PasswordGeneRateApp/models.py
+++ PasswordGeneRate/PasswordGeneRateApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Password(models.Model):
+    password = models.CharField(max_length = 100, unique = True)
+
+    def __str__(self):
+        return self.password
+    
+class Insult(models.Model):
+    insult = models.CharField(max_length = 255, unique = True)
+
+    def __str__(self):
+        return self.insult
